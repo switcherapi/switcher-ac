8de302926f397653b3dc73cbd3eb711ce09d98d2
petruki <31597636+petruki@users.noreply.github.com>
2022-03-31 22:35:28 -0700

    Closes #128: Upddate spring-boot to 2.5.12

--- src/main/java/com/github/switcherapi/ac/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/github/switcherapi/ac/config/JwtAuthenticationEntryPoint.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-@SuppressWarnings("serial")
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
 
