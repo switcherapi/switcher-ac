00f5686a95b748f3348cebb1b9be6a720d981744
petruki <31597636+petruki@users.noreply.github.com>
2022-04-25 20:26:40 -0700

    Closes #144 - Updated joda time to 2.10.14 - Fixed switcher test parameters

--- src/main/java/com/github/switcherapi/ac/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/github/switcherapi/ac/config/JwtAuthenticationEntryPoint.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+	
+	private static final long serialVersionUID = 7400943428027116206L;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
