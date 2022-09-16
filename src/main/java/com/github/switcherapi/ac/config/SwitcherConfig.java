b5b403eda3f9e6b5385b8f4a6356e3e3f9c4e825
petruki <31597636+petruki@users.noreply.github.com>
2022-09-15 20:44:52 -0700

    Added switcher-client url parameter

--- src/main/java/com/github/switcherapi/ac/config/SwitcherConfig.java
+++ src/main/java/com/github/switcherapi/ac/config/SwitcherConfig.java
@@ -14,7 +14,8 @@ import lombok.Data;
 @ConfigurationProperties(prefix = "switcher")
 @Data
 public class SwitcherConfig {
-	
+
+	private String url;
 	private String apikey;
 	private String domain;
 	private String component;
@@ -34,6 +35,7 @@ public class SwitcherConfig {
 	public void configureSwitcher() {
 		configure(ContextBuilder.builder()
 				.contextLocation(SwitcherFeatures.class.getName())
+				.url(url)
 				.apiKey(apikey)
 				.domain(domain)
 				.environment(environment)
