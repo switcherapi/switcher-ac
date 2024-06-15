1846bf25e06f187b30ff07728247df6a86c33748
petruki <31597636+petruki@users.noreply.github.com>
2024-06-15 16:01:32 -0700

    Added switcher-client scheduler logs - bump deps

--- src/main/java/com/github/switcherapi/ac/config/SwitcherConfig.java
+++ src/main/java/com/github/switcherapi/ac/config/SwitcherConfig.java
@@ -2,15 +2,19 @@ package com.github.switcherapi.ac.config;
 
 import com.github.switcherapi.ac.util.FileUtil;
 import com.github.switcherapi.client.ContextBuilder;
+import com.github.switcherapi.client.SnapshotCallback;
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import static com.github.switcherapi.ac.config.SwitcherFeatures.configure;
 import static com.github.switcherapi.ac.config.SwitcherFeatures.initializeClient;
+import static com.github.switcherapi.client.SwitcherContextBase.scheduleSnapshotAutoUpdate;
 
+@Slf4j
 @Configuration
 @ConfigurationProperties(prefix = "switcher")
 @Data
@@ -51,12 +55,18 @@ public class SwitcherConfig {
 				.component(component)
 				.local(local)
 				.silentMode(silent)
-				.snapshotAutoUpdateInterval(snapshot.getAutoUpdateInterval())
 				.snapshotLocation(StringUtils.isNotBlank(snapshot.getLocation()) ? snapshot.getLocation() : null)
 				.snapshotAutoLoad(snapshot.isAuto())
 				.truststorePath(StringUtils.isNotBlank(truststore.getPath()) ? FileUtil.getFilePathFromResource(truststore.getPath()) : null)
 				.truststorePassword(StringUtils.isNotBlank(truststore.getPassword()) ? truststore.getPassword() : null)
 		);
+
+		scheduleSnapshotAutoUpdate(snapshot.getAutoUpdateInterval(), new SnapshotCallback() {
+			@Override
+			public void onSnapshotUpdate(long version) {
+				log.info("Snapshot updated: {}", version);
+			}
+		});
 
 		initializeClient();
 	}
