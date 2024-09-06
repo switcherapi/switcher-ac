3f77a032885ed172145e0c05ce98bc916b2c9068
petruki <31597636+petruki@users.noreply.github.com>
2024-09-05 22:22:17 -0700

    chore: removed unused imports

--- src/main/java/com/github/switcherapi/ac/model/dto/RequestRelayDTO.java
+++ src/main/java/com/github/switcherapi/ac/model/dto/RequestRelayDTO.java
@@ -3,8 +3,6 @@ package com.github.switcherapi.ac.model.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import lombok.Data;
-
 @JsonInclude(Include.NON_NULL)
 public record RequestRelayDTO(
 		String value,
