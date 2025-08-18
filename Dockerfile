f0e6e358f35bfa89a83f7e5876d6998457dd92fb
petruki <31597636+petruki@users.noreply.github.com>
2025-08-17 17:48:35 -0700

    Upgraded GraalVM compiler to 24.0.2, updated metadata reach tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.9.9-eclipse-temurin-21-alpine AS maven
+FROM maven:3.9.11-eclipse-temurin-21-alpine AS maven
 
 WORKDIR /usr/src/app
 COPY . /usr/src/app
--- src/test/java/metainf/nativeimage/NativeReflectConfigTest.java
+++ src/test/java/metainf/nativeimage/NativeReflectConfigTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -41,8 +42,7 @@ class NativeReflectConfigTest {
 		ReflectJson[] reflectJson = new Gson().fromJson(reflectContent, ReflectJson[].class);
 
 		for (ReflectJson json : reflectJson) {
-			assertDoesNotThrow(() -> Class.forName(json.name),
-					String.format("Class %s not reachable", json.name));
+			assertConstructorsReachable(json);
 
 			if (Objects.nonNull(json.condition)) {
 				assertDoesNotThrow(() -> Class.forName(json.condition.typeReachable),
@@ -51,12 +51,65 @@ class NativeReflectConfigTest {
 		}
 	}
 
+	private void assertConstructorsReachable(ReflectJson json) {
+		if (Objects.nonNull(json.methods)) {
+			for (ReflectMethod method : json.methods) {
+				if ("<init>".equals(method.name)) {
+					assertDoesNotThrow(() -> Class.forName(json.name).getDeclaredConstructor(getArgClasses(method.parameterTypes)),
+							String.format("Constructor (%s) not reachable in class %s",
+									String.join(", ", method.parameterTypes), json.name));
+				}
+			}
+		}
+	}
+
+	private Class<?>[] getArgClasses(String[] parameterTypes) throws ClassNotFoundException {
+		if (parameterTypes == null || parameterTypes.length == 0) {
+			return new Class<?>[0];
+		}
+
+		Class<?>[] classes = new Class<?>[parameterTypes.length];
+		for (int i = 0; i < parameterTypes.length; i++) {
+			String paramType = parameterTypes[i];
+			if (paramType.endsWith("[]")) {
+				String baseType = paramType.substring(0, paramType.length() - 2);
+				Class<?> baseClass = getPrimitiveOrClass(baseType);
+				classes[i] = Array.newInstance(baseClass, 0).getClass();
+			} else {
+				classes[i] = getPrimitiveOrClass(paramType);
+			}
+		}
+
+		return classes;
+	}
+
+	private Class<?> getPrimitiveOrClass(String typeName) throws ClassNotFoundException {
+		return switch (typeName) {
+			case "boolean" -> boolean.class;
+			case "byte" -> byte.class;
+			case "char" -> char.class;
+			case "short" -> short.class;
+			case "int" -> int.class;
+			case "long" -> long.class;
+			case "float" -> float.class;
+			case "double" -> double.class;
+			case "void" -> void.class;
+			default -> Class.forName(typeName);
+		};
+	}
+
 	static class ReflectJson {
 		String name;
 		ReflectCondition condition;
+		ReflectMethod[] methods;
 	}
 
 	static class ReflectCondition {
 		String typeReachable;
+	}
+
+	static class ReflectMethod {
+		String name;
+		String[] parameterTypes;
 	}
 }
